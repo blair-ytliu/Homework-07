91b536b18330351a770d6a70ea8fcc92b83d575b
blair-ytliu <blair.ytliu@gmail.com>
2016-11-11 12:01:59 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function random(m){
+    return Math.floor(Math.random()*m+1);
+}
+
+function max(a,b){
+    if(a>b) return a;
+    else return b;
+}
+    
+function lastChar(arr) {
+    return arr[arr.length - 1];
+
+}
+function squares(a, b){
+    return (a*a + b*b);
+}
